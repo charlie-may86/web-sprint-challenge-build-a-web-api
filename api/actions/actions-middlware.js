492f4f55e44769a2d7ede476e5d7d4ad668e4654
Charlie May <61992595+charlie-may86@users.noreply.github.com>
2021-09-12 13:46:49 -0500

    more done

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,27 @@
 // add middlewares here related to actions
+
+const Action = require("./actions-model");
+
+function validateActionId(req, res, next) {
+  const id = req.params.id;
+
+  Action.get(id).then((action) => {
+    if (action) {
+      req.action = action;
+      next();
+    } else {
+      res.status(404).json({ message: "action not found" });
+    }
+  });
+}
+
+function validateAction(req, res, next) {
+  const { project_id, description, notes } = req.body;
+  if (!project_id || !description || !notes) {
+    res.status(400).json({ message: "missing required field" });
+  } else {
+    next();
+  }
+}
+
+module.exports = { validateActionId, validateAction };
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,10 +3,32 @@ const express = require("express");
 const router = express.Router();
 const Action = require("./actions-model");
 
+const { validateActionId, validateAction } = require("./actions-middlware");
+
 router.get("/", (req, res, next) => {
   Action.get()
     .then((actions) => {
       res.status(200).json(actions);
+    })
+    .catch(next);
+});
+
+router.get("/:id", validateActionId, (req, res) => {
+  res.json(req.action);
+});
+
+router.post("/", validateAction, (req, res, next) => {
+  Action.insert(req.body)
+    .then((newAction) => {
+      res.status(201).json(newAction);
+    })
+    .catch(next);
+});
+
+router.put("/:id", validateActionId, validateAction, (req, res, next) => {
+  Action.update(req.params.id, req.body)
+    .then((updatedAction) => {
+      res.status(200).json(updatedAction);
     })
     .catch(next);
 });
